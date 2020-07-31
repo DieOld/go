c3b6897c4f2bbdf4b33d60bd73b83576e8e12c4f
vadym.tishchenko <vadym.tishchenko@raccoongang.com>
2020-07-31 16:59:53 +0300

    Add tests

--- eulersProject/solutions/euler_1.go
+++ eulersProject/solutions/euler_1.go
@@ -2,13 +2,9 @@
 	Find sum of numbers that lcm of 3 and 5
 */
 
-package main
+package solutions
 
-import (
-	"fmt"
-)
-
-func euler1(limit int) int {
+func Euler1(limit int) int {
 	sum := 0
 	for i := 3; i < limit; i++ {
 		if i%5 == 0 || i%3 == 0 {
@@ -16,8 +12,4 @@ func euler1(limit int) int {
 		}
 	}
 	return sum
-}
-
-func main() {
-	fmt.Println(euler1(1000))
 }
