6ca2befc12eb90372608c1445b2263d17fa2a9c5
vadym.tishchenko <vadym.tishchenko@raccoongang.com>
2020-07-31 12:50:22 +0300

    Add eulers project solution N1

---
+++ eulersProject/euler_1.go
@@ -0,0 +1,23 @@
+/*
+	Find sum of numbers that lcm of 3 and 6
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func euler1(limit int) int {
+	sum := 0
+	for i := 3; i < limit; i++ {
+		if i%5 == 0 || i%3 == 0 {
+			sum += i
+		}
+	}
+	return sum
+}
+
+func main() {
+	fmt.Println(euler1(1000))
+}
